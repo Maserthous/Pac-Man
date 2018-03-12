9b3ae640ffa8eed25d697052489a81f4d304d364
Maserthous <35930761+Maserthous@users.noreply.github.com>
2018-03-12 01:32:25 -0400

    Added Teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public float offset;
+
+    private Transform trans;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player")
+        {
+            Vector3 pos = other.gameObject.GetComponent<Transform>().position;
+
+            if (pos.x >= 0)
+            {
+                pos.x = -pos.x + offset;
+            }
+            else
+            {
+                pos.x = -pos.x - offset;
+            }
+
+            other.gameObject.GetComponent<Transform>().position = pos;
+        }
+    }
+}
